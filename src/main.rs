04084eca0223cbb9d7e425fcbcdad473c5f17324
Adrian <adrian-holz@mailbox.org>
2024-03-08 20:25:05 +0100

    Log wifi data

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use std::str::FromStr;
+use std::{thread::sleep, time::Duration};
+
+use embedded_svc::wifi::{ClientConfiguration, Configuration};
+use esp_idf_svc::hal::peripherals::Peripherals;
+use esp_idf_svc::wifi::EspWifi;
+use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
+
 fn main() {
-    // It is necessary to call this function once. Otherwise some patches to the runtime
+    // It is necessary to call this function once. Otherwise, some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
 
@@ -7,4 +15,33 @@ fn main() {
     esp_idf_svc::log::EspLogger::initialize_default();
 
     log::info!("Hello, world!");
+
+    let peripherals = Peripherals::take().unwrap();
+    let sys_loop = EspSystemEventLoop::take().unwrap();
+    let nvs = EspDefaultNvsPartition::take().unwrap();
+
+    let mut wifi_driver = EspWifi::new(peripherals.modem, sys_loop, Some(nvs)).unwrap();
+
+    wifi_driver
+        .set_configuration(&Configuration::Client(ClientConfiguration {
+            ssid: heapless::String::from_str("YOUR_WIFI_SSID").unwrap(),
+            password: heapless::String::from_str("YOUR_WIFI_PASSWORD").unwrap(),
+            ..Default::default()
+        }))
+        .unwrap();
+
+    wifi_driver.start().unwrap();
+    wifi_driver.connect().unwrap();
+    while !wifi_driver.is_connected().unwrap() {
+        let config = wifi_driver.get_configuration().unwrap();
+        log::info!("Waiting for station {:?}", config);
+    }
+    log::info!("Should be connected now");
+    loop {
+        log::info!(
+            "IP info: {:?}",
+            wifi_driver.sta_netif().get_ip_info().unwrap()
+        );
+        sleep(Duration::new(10, 0));
+    }
 }
